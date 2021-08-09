ef07e89035870fa9acaf76f7d985171de0b28c11
ernesto <ernesto002@icloud.com>
2021-08-09 11:13:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
+
+
+let url = 'https://anapioficeandfire.com/api/books'
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  getPosts()
+  return fetch(url)
+  .then(function(response) {
+    return response.json() 
+ })  
+ 
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  fetchBooks()
+})
+
+
+function getPosts(){
+  return fetch(url)
+  .then(function(response) {
+    return response.json() 
+ }).then(function(data){
+  renderBooks(data)
+ })
+ 
+  
+}
